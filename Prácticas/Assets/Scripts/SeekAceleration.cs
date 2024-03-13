40d160e5078535b4527f3b427ee35065cf6710dd
Luis Eduardo Maiquez <luiseduardo.maiquez@ticarum.es>
2024-03-13 15:02:10 +0100

    Cámaras añadidas e implementación inicial del Gizmo

--- Prácticas/Assets/Scripts/SeekAceleration.cs
+++ Prácticas/Assets/Scripts/SeekAceleration.cs
@@ -29,4 +29,37 @@ public class Seek1 : MonoBehaviour
 
         transform.position += velocity * Time.deltaTime;
     }
+
+    // Dibuja el gizmo de la velocidad y de la orientación
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.blue;
+        // El From es normalmente donde se encuentra el personaje. De hecho, es el mismo para velocidad y orientación
+ //       Vector3 from = transform.position;
+
+ //       Vector3 newDirection = target.position - transform.position;
+
+  //      Vector3 velocityGiz = newDirection * maxAceleration * 2f;
+
+   //     Vector3 velTo = transform.localPosition + velocityGiz;
+   //     Vector3 orienTo = newDirection;
+        
+
+        Vector3 velActual = new Vector3(-5, 0, 3);
+        Debug.Log("Velocidad: " + velActual);
+
+        // Elevación para no tocar el suelo
+        Vector3 elevation = new Vector3 (0 , 1 , 0) ;
+        Vector3 from = transform.position + elevation;
+        Vector3 to = from + velActual + elevation;
+       // Vector3 orienTo = orienTo + elevation;
+        Debug.Log("From: " + from);
+        Gizmos.DrawLine (from , to);
+
+        // Gizmos.color = Color.black;
+        // Gizmos.DrawLine (from , orienTo);
+
+    }
+
 }
