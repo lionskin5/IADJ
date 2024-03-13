aeaeae5fdc486ac9cf716d86cfdc2005312f67c6
lionskin5 <luiseduardo.maiquez@gmail.com>
2024-03-13 19:40:44 +0100

    Finalizando el gizmo utilizando el paquete del aula virtual

--- Prácticas/Assets/Scripts/SeekAceleration.cs
+++ Prácticas/Assets/Scripts/SeekAceleration.cs
@@ -33,32 +33,24 @@ public class Seek1 : MonoBehaviour
     // Dibuja el gizmo de la velocidad y de la orientación
     private void OnDrawGizmos()
     {
+        Vector3 from = transform.position; // Origen de la línea
+        Vector3 to = transform.position + velocity; // Destino de la línea
+        Vector3 elevation = new Vector3(0, 1, 0); // Elevación para no tocar el suelo
 
-        Gizmos.color = Color.blue;
-        // El From es normalmente donde se encuentra el personaje. De hecho, es el mismo para velocidad y orientación
- //       Vector3 from = transform.position;
+        from = from + elevation;
+        to = to + elevation;
 
- //       Vector3 newDirection = target.position - transform.position;
-
-  //      Vector3 velocityGiz = newDirection * maxAceleration * 2f;
-
-   //     Vector3 velTo = transform.localPosition + velocityGiz;
-   //     Vector3 orienTo = newDirection;
-        
-
-        Vector3 velActual = new Vector3(-5, 0, 3);
-        Debug.Log("Velocidad: " + velActual);
-
-        // Elevación para no tocar el suelo
-        Vector3 elevation = new Vector3 (0 , 1 , 0) ;
-        Vector3 from = transform.position + elevation;
-        Vector3 to = from + velActual + elevation;
-       // Vector3 orienTo = orienTo + elevation;
+        Debug.Log("Velocidad: " + velocity);
         Debug.Log("From: " + from);
-        Gizmos.DrawLine (from , to);
+        Debug.Log("To: " + to);
 
-        // Gizmos.color = Color.black;
-        // Gizmos.DrawLine (from , orienTo);
+        Gizmos.color = Color.yellow;   // Mirando en la dirección de la velocidad.
+        Gizmos.DrawLine(from, to);
+
+        Gizmos.color = Color.red;   // Mirando en la dirección de la orientación.
+        Vector3 direction = transform.TransformDirection(Vector3.forward) * 5;
+        Debug.Log("Direction: " + direction);
+        Gizmos.DrawRay(from, direction);
 
     }
 
